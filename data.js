d68b5a43c388b86c1f15ab8bb1e50e23d7a48165
Elizeu <elizeucoca@gmail.com>
2021-07-23 22:16:36 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    title:
+      "Como comecei a programar / 5 dicas de aprendizado/ dicas GitHub",
+    artist: "Elizeu Lima",
+    cover: "como-comecei.jpg",
+    file: "como-comecei-lima.mp3"
+  },
+  {
+    title: "5 dicas para Começar",
+    artist: "Elizeu Lima",
+    cover: "5-dicas-Alle-ao-Lado.jpg",
+    file: "dicaslima.mp3"
+  },
+  {
+    title: "Repositorio Iniciante no Github",
+    artist: "Elizeu Lima",
+    cover: "github.png",
+    file: "podcastgithub.mp3"
+  }
+];
